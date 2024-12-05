025ccbcae60524a6ee07d18056465bf5b5ae2ecf
FreecoMwaniki <freecomwaniki@gmail.com>
2024-12-05 19:35:46 +0300

    First Commut

---
+++ app/src/main/java/com/example/freecodemo/SkatingActivity.kt
@@ -0,0 +1,22 @@
+package com.example.freecodemo
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class SkatingActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState, )
+        setContentView(R.layout.activity_main)
+        val button: Button =findViewById(R.id.main)
+        button.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+//            intent.putExtra("key", "Welcome $nameText")
+            startActivity(intent)}
+
+
+        }
+
+
+    }
